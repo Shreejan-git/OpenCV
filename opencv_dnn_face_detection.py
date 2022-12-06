83694f51b1be0e0b93ccc6057ee76074c0b45cdb
shreejan_git <hustlingshreejan@gmail.com>
2022-12-06 12:16:07 +0545

    implement opencv dnn and dlib 5-points landmark detection

--- opencv_dnn_face_detection.py
+++ opencv_dnn_face_detection.py
@@ -1,40 +1,71 @@
 import numpy as np
+import dlib
 import cv2
+import imutils
+from imutils import face_utils
 
-
-modelFile = "res10_300x300_ssd_iter_140000.caffemodel"
-configFile = "deploy.prototxt.txt"
-
-
-def opencv_dnn_image(configfile, modelfile, image):
+def opencv_dnn_image(configfile, modelfile, image, threshold=0.9):
     modelFile = modelfile
     configFile = configfile
 
     net = cv2.dnn.readNetFromCaffe(configFile, modelFile)
 
-    image = cv2.imread('images/tilted_head.jpg')
+    image = cv2.imread(image)
+    image = cv2.resize(image,(300,300))
+    # print(image.shape)
     (h, w) = image.shape[:2]
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    blob = cv2.dnn.blobFromImage(cv2.resize(
-        image, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
+    # we will have an image of size(h,w,3) but Opencv DNN excepts it to be (1,3,300,300)
+    blob = cv2.dnn.blobFromImage(image, scalefactor=1.0, size=(300, 300), mean=(104.0, 177.0, 123.0))
+    # print(blob.shape)
 
+    # We can feed the processed image to the caffe model now. This is a basic feed forward step in neural networks.
     net.setInput(blob)
     detections = net.forward()
+    predictor = dlib.shape_predictor('shape_predictor_5_face_landmarks.dat')
 
-    for i in range(0, detections.shape[2]):
 
-        confidence = detections[0, 0, i, 2]
-
-        if confidence > 0.8:
-            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-            (startX, startY, endX, endY) = box.astype("int")
+    for i in detections[0, 0]:
+        confidence = i[2]
+        if confidence > threshold:
+            left = (i[3] * 300).astype(int)
+            top = (i[4] * 300).astype(int)
+            right = (i[5] * 300).astype(int)
+            bottom = (i[6] * 300).astype(int)
+            cv2.rectangle(image, (left, top+10),
+                          (right, bottom), (0, 0, 255), 2)
             text = "{:.2f}%".format(confidence * 100)
-            y = startY - 10 if startY - 10 > 10 else startY + 10
-            cv2.rectangle(image, (startX, startY), (endX, endY),
-                          (0, 0, 255), 2)
-            cv2.putText(image, text, (startX, y),
+            cv2.putText(image, text, (left, top),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+            
+            rect = dlib.rectangle(left, top, right, bottom)
+            # print(rect)
+            shape = predictor(gray, rect)
+            shape = face_utils.shape_to_np(shape)
+
+            for (i, (x, y)) in enumerate(shape):
+                cv2.circle(image, (x, y), 1, (0, 0, 255), -1)
+                cv2.putText(image, str(i + 1), (x - 10, y - 10),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 1)
+            
+
 
     # show the output image
     cv2.imshow("Output OpenCV DNN", image)
     cv2.waitKey(0)
+    
+
+
+
+
+
+if __name__ == '__main__':
+
+    image = 'images/double_head.jpg'
+    # image = 'images/tilted_head.jpg'
+
+    modelFile = "res10_300x300_ssd_iter_140000.caffemodel"
+    configFile = "deploy.prototxt.txt"
+
+    opencv_dnn_image(configFile, modelFile, image)
