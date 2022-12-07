b0b3f8c033db82b3aa98d4ad6725741525acf7a0
shreejan_git <hustlingshreejan@gmail.com>
2022-12-07 14:32:00 +0545

    implement face detection and alignment using MTCNN

--- MTCNN_face_detection.py
+++ MTCNN_face_detection.py
@@ -2,32 +2,17 @@ from mtcnn import MTCNN
 import cv2
 import time
 import os
-import uuid
 import numpy as np
 
 
 def align_face_mtcnn(img, left_eye, right_eye):
 
-    desiredLeftEye = (0.38, 0.38)
-    desiredFaceWidth = 256
+    desiredLeftEye = (0.39, 0.39)
+    desiredFaceWidth = 300
     desiredFaceHeight = None
 
     if desiredFaceHeight is None:
         desiredFaceHeight = desiredFaceWidth
-
-    # if left_eye[1] > right_eye[1]:
-    #     # -1 indicates the image will rotate in the clockwise direction
-    #     third_point = (right_eye[0], left_eye[1])
-    #     # cv2.circle(img, third_point, 2, (255, 0, 0), 2)
-    # else:
-    #     third_point = (left_eye[0], right_eye[1])
-    #     # 1 indicates the image will rotate in the counter clockwise direction
-    #     # cv2.circle(img, third_point, 2, (255, 0, 0), 2)
-    #     direction = 1
-
-    # cv2.line(img, right_eye, left_eye, (0, 255, 0), 2)
-    # cv2.line(img, left_eye, third_point, (255, 0, 0), 2)
-    # cv2.line(img, right_eye, third_point, (0, 0, 255), 2)
 
     delta_x = right_eye[0] - left_eye[0]
     delta_y = right_eye[1] - left_eye[1]
@@ -35,15 +20,22 @@ def align_face_mtcnn(img, left_eye, right_eye):
     angle = (angle * 180) / np.pi
 
     desiredRightEyeX = 1.0 - desiredLeftEye[0]
+
     dist = np.sqrt((delta_x ** 2) + (delta_y ** 2))
+
     desiredDist = (desiredRightEyeX - desiredLeftEye[0])
+
     desiredDist *= desiredFaceWidth
+
     scale = desiredDist / dist
 
     eyesCenter = ((left_eye[0] + right_eye[0]) // 2,
                   (left_eye[1] + right_eye[1]) // 2)
 
+    # cv2.circle(img, (eyesCenter[0], eyesCenter[1]), 3, (255, 0, 255), -1)
+
     M = cv2.getRotationMatrix2D(eyesCenter, (angle), scale)
+
     tX = desiredFaceWidth * 0.5
     tY = desiredFaceHeight * desiredLeftEye[1]
     M[0, 2] += (tX - eyesCenter[0])
@@ -56,7 +48,7 @@ def align_face_mtcnn(img, left_eye, right_eye):
     return rotated
 
 
-def mtcnn_face_detector_image(img, path):
+def mtcnn_face_detector_image(originalimg, path, confidence=0.8):
     '''
     The detector returns a list of JSON objects. Each JSON object contains three main keys: 'box', 'confidence' and 'keypoints':
 
@@ -73,18 +65,18 @@ def mtcnn_face_detector_image(img, path):
 
     detector = MTCNN()
 
+    img = originalimg
     img = cv2.imread(img)
     rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
     result = detector.detect_faces(rgb)
-    print(result)
 
     for i in result:
-        if i['confidence'] > 0.8: #setting the threshold
-            
-            #Uncomment below code to see the B.box of detected face and the landmarks
-            # x, y, w, h = i['box']
-            # img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 1)
+        if i['confidence'] > confidence:  # setting the threshold
+
+            # Uncomment below code to see the B.box of detected face and the landmarks
+            x, y, w, h = i['box']
+            # cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 1)
 
             # c = 0
             # for (x, y) in i['keypoints'].values():
@@ -98,45 +90,51 @@ def mtcnn_face_detector_image(img, path):
             # cv2.line(img, right_eye, left_eye, (67, 67, 67), 2)
 
             rotated = align_face_mtcnn(
-                img, left_eye, right_eye)  # face alignment
-
+                    img, left_eye, right_eye)  # face alignment
             cv2.imshow('rotated', rotated)
+            cv2.imshow('MTCNN output', img)
             cv2.waitKey(0)
 
-        #Below code will give the new image with required area of interest
-        # path = os.path.join(path, f'{str(uuid.uuid1())}.jpg')
-        # cv2.imwrite(path, img)
+        # Below code will give the new image with required area of interest
+        img_name = originalimg.split('/')[-1].split('.')[0]
+        path = os.path.join(path, f'{img_name+str(np.random.randint(0,100))}.jpg')
+        # cv2.imwrite(path, rotated)
+
+
+def main_fun(base_dir):
+    sub_dir = os.listdir(base_dir)
+    start = time.time()
+    for sub in sub_dir:
+        sub_path = os.path.join(base_dir, sub)
+        images = os.listdir(sub_path)
+        output_dir = os.path.join(sub_path, sub + '_mtcnn')
+
+        for image in images:
+            image_path = os.path.join(sub_path, image)
+
+            if not os.path.exists(output_dir):
+                os.mkdir(output_dir)
+
+            if os.path.exists(image_path):
+                mtcnn_face_detector_image(
+                    image_path, output_dir)  # path and img
+            else:
+                continue
+
+    end = time.time()
+    print('The detection time for MTCNN was', (end-start))
 
 
 if __name__ == '__main__':
 
-    base_dir = 'celebrity'
-    sub_dir = os.listdir(base_dir)
+    base_dir = 'celebraty'
 
-    img = 'images/tilthead_second.jpg'
-    img = 'images/tilted_head.jpg'
+    # testing images
+    # img = 'images/tilted_head_second.jpg'
+    # img = 'images/tilted_head_third.jpg'
+    # img = 'images/animated.jpg'
+    # img = 'images/double_head.jpg'
+    # img = 'images/pryinka_karki5.jpg'
+    # mtcnn_face_detector_image(img, 7)
 
-    mtcnn_face_detector_image(img, 7)
-
-    # start = time.time()
-    # for sub in sub_dir:
-    #     sub_path = os.path.join(base_dir, sub)
-    #     images = os.listdir(sub_path)
-
-    #     for image in images:
-    #         image_path = os.path.join(sub_path, image)
-
-    #         output_dir = os.path.join(sub_path, 'mtcnn')
-
-    #         if not os.path.exists(output_dir):
-    #             os.mkdir(output_dir)
-
-    #         if os.path.exists(image_path):
-    #             mtcnn_face_detector_image(
-    #                 image_path, output_dir)  # path and img
-    #             break
-    #         else:
-    #             continue
-
-    # end = time.time()
-    # print('The detection time for MTCNN was', (end-start))
+    main_fun(base_dir)
