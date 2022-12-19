9e93d18494cfa2e5f549d6317c9c40a408d58736
shreejan_git <hustlingshreejan@gmail.com>
2022-12-19 16:20:49 +0545

    implement embedding function

--- siamese_fr.py
+++ siamese_fr.py
@@ -61,3 +61,109 @@ def data_collector(ANC_PATH, POS_PATH, name):
     cv2.destroyAllWindows()
     
 # data_collector(ANC_PATH, POS_PATH, 'shreejan')
+
+#taking 60 images of each class
+anchor = tf.data.Dataset.list_files(ANC_PATH + '/*.jpg').take(60)
+positive = tf.data.Dataset.list_files(POS_PATH + '/*.jpg').take(60)
+negative = tf.data.Dataset.list_files(NEG_PATH +'/*.jpg').take(60)
+
+# generator_ = anchor.as_numpy_iterator()
+# print(generator_.next())
+
+def image_preprocess(img_path):
+    '''
+    reading the image and resizing it to (105,105). 
+    (105,105) is the recommended size in the paper.
+    '''
+    image = tf.io.read_file(img_path)
+    img = tf.io.decode_jpeg(image)
+    
+    #scaling (normalizing) the image to 0-1 pixel value
+    img = tf.image.resize(img, (105,105))
+    
+    img = img/255.0
+    
+    return img
+
+#labeling the image, i.e. (anchor, positive == 1) $ (anchor, negative ==0)
+positives = tf.data.Dataset.zip((anchor, positive, tf.data.Dataset.from_tensor_slices(tf.ones(len(anchor)))))
+negatives = tf.data.Dataset.zip((anchor, negative, tf.data.Dataset.from_tensor_slices(tf.zeros(len(anchor)))))
+data = positives.concatenate(negatives) #this contains the data in the form of generator
+# samples = data.as_numpy_iterator()
+# print(samples.next())
+
+def preprocess_twin(input_img, validation_img, label):
+    return (image_preprocess(input_img), image_preprocess(validation_img), label)
+
+
+data = data.map(preprocess_twin)
+data = data.cache()
+data = data.shuffle(buffer_size=60)
+
+#training partition
+train_data = data.take(round(len(data)*.7))
+train_data = train_data.batch(16)
+train_data = train_data.prefetch(8)
+
+train_samples = train_data.as_numpy_iterator()
+train_sample = train_samples.next()
+print(len(train_sample[0]))
+
+#testing partition
+test_data = data.skip(round(len(data)*.7))
+test_data = test_data.take(round(len(data)*.3))
+test_data = test_data.batch(16)
+test_data = test_data.prefetch(8)
+
+def make_embeddings():
+    inp = Input(shape=(100,100,3), name='input_image')
+    
+    c1 = Conv2D(64, (10,10), activation='relu')(inp)
+    m1 = MaxPool2D(64, (2,2), padding='same')(c1)
+    
+    c2 = Conv2D(64, (7,7), activation='relu')(m1) #originally there was 128 filters
+    m2 = MaxPool2D(64, (2,2), padding='same')(c2)
+    
+    c3 = Conv2D(64, (4,4), activation='relu')(m2) #originally there was 128 filters
+    m3 = MaxPool2D(64, (2,2), padding='same')(c3)
+    
+    c4 = Conv2D(128, (4,4), activation='relu')(m3) #originally there was 255 filters
+    
+    f1 = Flatten()(c4)
+    
+    d1 = Dense(4096, activation='sigmoid',)(f1)
+    
+    return Model(inputs=[inp], outputs=[d1], name='embedding')
+    
+# model = make_embeddings()
+# print(model.summary())
+
+class L1Dist(Layer):
+    def __init__(self, **kwargs):
+        super().__init__()
+        
+    def call(self, input_embedding, validation_embedding):
+        return tf.math.abs(input_embedding, validation_embedding)
+        
+
+def make_siamese_network():
+    input_image = Input(shape=(100,100,3), name='input_img')
+    
+    validation_image = Input(shape=(100,100,3), name='validation_img')
+    
+    siamese_layer = L1Dist()
+    siamese_layer._name = 'distance'
+    distances = siamese_layer(make_embeddings(input_image), make_embeddings(validation_image))
+    
+    classifier = Dense(1, activation='sigmoid')(distances)
+    
+    return Model(inputs=[input_image, validation_image], outputs=[classifier], name='SiameseNetwork')
+    
+binary_cross_loss = tf.losses.BinaryCrossentropy() #from_logit = True 
+
+
+
+
+
+
+
