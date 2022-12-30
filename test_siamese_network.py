b16d67c05a0fa61836722ab7f5b4a40e336eba10
shreejan_git <hustlingshreejan@gmail.com>
2022-12-30 14:46:10 +0545

    implement test portion for siamese NN

---
+++ test_siamese_network.py
@@ -0,0 +1,63 @@
+import config
+import utils
+from keras.models import load_model
+from imutils.paths import list_images
+import numpy as np
+import cv2
+import tensorflow as tf
+
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+print("[INFO] loading test dataset...")
+# testImagePaths = testX
+testImagePaths = list(list_images('examples'))
+print(testImagePaths)
+np.random.seed(42)
+pairs = np.random.choice(testImagePaths, size=(10, 2))
+print(pairs)
+
+# # load the model from disk
+# print("[INFO] loading siamese model...")
+model = load_model(config.MODEL_PATH)
+
+for (i, (pathA, pathB)) in enumerate(pairs):
+    # load both the images and convert them to grayscale
+    imageA = cv2.imread(pathA, 0)
+    imageB = cv2.imread(pathB, 0)
+    
+    # create a copy of both the images for visualization purpose
+    origA = imageA.copy()
+    origB = imageB.copy()
+    
+    #resizing the image according to the model's input requirement
+    
+    imageA = cv2.resize(imageA, (28,28))
+    imageB = cv2.resize(imageB, (28,28))
+    
+    # add channel a dimension to both the images
+    imageA = np.expand_dims(imageA, axis=-1)
+    imageB = np.expand_dims(imageB, axis=-1)
+    # add a batch dimension to both images
+    imageA = np.expand_dims(imageA, axis=0)
+    imageB = np.expand_dims(imageB, axis=0)
+    # scale the pixel values to the range of [0, 1]
+    imageA = imageA / 255.0
+    imageB = imageB / 255.0
+    # use our siamese model to make predictions on the image pair,
+    # # indicating whether or not the images belong to the same class
+    preds = model.predict([imageA, imageB])
+    proba = preds[0][0]
+    fig = plt.figure("Pair #{}".format(i + 1), figsize=(4, 2))
+    plt.suptitle("Similarity: {:.2f}".format(proba))
+    # show first image
+    ax = fig.add_subplot(1, 2, 1)
+    plt.imshow(origA, cmap=plt.cm.gray)
+    plt.axis("off")
+    # show the second image
+    ax = fig.add_subplot(1, 2, 2)
+    plt.imshow(origB, cmap=plt.cm.gray)
+    plt.axis("off")
+    # show the plot
+    plt.show()
